9cc011e18bd266d7636ef2ea8d8e5e619a97fbd1
Youri Bijen <youri.bijen@iquality.nl>
2020-01-24 14:40:47 +0100

    Transferred day 2 from another repository

--- Solutions/Year2019/Day02/Solution.cs
+++ Solutions/Year2019/Day02/Solution.cs
@@ -1,17 +1,92 @@
-namespace AdventOfCode.Solutions.Year2019 {
+using System.Collections.Generic;
+using System.Linq;
 
-    class Day02 : ASolution {
+namespace AdventOfCode.Solutions.Year2019
+{
+    class Day02 : ASolution
+    {
+        private const int OPCODE_ADD = 1;
+        private const int OPCODE_MULTIPLY = 2;
+        private const int OPCODE_STOP = 99;
+        private const int INDEX_INCREMENT = 4;
 
-        public Day02() : base(2, 2019, "") {
-
-        }
+        public Day02() : base(2, 2019, "") { }
 
         protected override string SolvePartOne() {
-            return null;
+            var input = Input.Split(",").Select(v => int.Parse(v)).ToList();
+
+            // Manual override for part 1
+            input[1] = 1;
+            input[2] = 0;
+
+            return PerformSteps(input)[0].ToString();
+        }
+        protected override string SolvePartTwo()
+        {
+            var input = Input.Split(",").Select(v => int.Parse(v)).ToList();
+
+            return FindNounAndVerbPartTwo(input, 19690720).ToString();
         }
 
-        protected override string SolvePartTwo() {
-            return null;
+        public List<int> PerformSteps(List<int> input)
+        {
+            var currentIndex = 0;
+            while (true)
+            {
+                var opcode = input[currentIndex];
+                if (opcode == OPCODE_STOP)
+                {
+                    break;
+                }
+
+                var value1 = GetValueOfGivenIndex(input, currentIndex + 1);
+                var value2 = GetValueOfGivenIndex(input, currentIndex + 2);
+                var updateIndex = input[currentIndex + 3];
+
+                if (opcode == OPCODE_ADD)
+                {
+                    input[updateIndex] = value1 + value2;
+                }
+                else if (opcode == OPCODE_MULTIPLY)
+                {
+                    input[updateIndex] = value1 * value2;
+                }
+
+                currentIndex += INDEX_INCREMENT;
+            }
+
+            return input;
+        }
+
+        private int GetValueOfGivenIndex(List<int> input, int index) => input[input[index]];
+
+        private int FindNounAndVerbPartTwo(List<int> input, int valueToFind)
+        {
+            // Keep a copy of the original input
+            var originalInput = input.ToArray();
+
+            var currentNoun = 0;
+            while (true)
+            {
+                input[1] = currentNoun;
+                if (PerformSteps(input)[0] > valueToFind)
+                {
+                    currentNoun--;
+
+                    // Obtain the value of the current Noun (the previous in the loop)
+                    input = originalInput.ToList();
+                    input[1] = currentNoun;
+
+                    // Calculate the verb
+                    var verb = valueToFind - PerformSteps(input)[0];
+
+                    var answer = 100 * currentNoun + verb;
+                    return answer;
+                }
+
+                currentNoun++;
+                input = originalInput.ToList();
+            }
         }
     }
 }
---
+++ Solutions/Year2019/Day02/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Solutions.Year2019
+{
+    public class Day02_Tests
+    {
+        private readonly Day02 _day02;
+
+        public Day02_Tests()
+        {
+            _day02 = new Day02();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 0, 0, 0, 99 }, new int[] { 2, 0, 0, 0, 99 })]
+        public void AoCExamples_Part1(IEnumerable<int> input, IEnumerable<int> expected)
+        {
+            Assert.True(_day02.PerformSteps(input.ToList()).SequenceEqual(expected));
+        }
+    }
+}
