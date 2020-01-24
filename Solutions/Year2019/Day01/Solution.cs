0fd0c6469395d99ff1a8c50bb9ad51a64b3d09c7
Youri Bijen <youri.bijen@iquality.nl>
2020-01-24 12:20:37 +0100

    Transferred day 1 from another repository

--- Solutions/Year2019/Day01/Solution.cs
+++ Solutions/Year2019/Day01/Solution.cs
@@ -1,17 +1,42 @@
-namespace AdventOfCode.Solutions.Year2019 {
+using System;
+using System.Linq;
 
+namespace AdventOfCode.Solutions.Year2019
+{
     class Day01 : ASolution {
+        
+        private const int DIVIDE_BY = 3;
+        private const int REDUCE_BY = 2;
 
-        public Day01() : base(1, 2019, "") {
+        public Day01() : base(1, 2019, "") { }
 
+        protected override string SolvePartOne() =>
+            Input.SplitByNewline().Select(v => double.Parse(v)).Sum(CalculateRequiredFuel).ToString();
+
+        protected override string SolvePartTwo() =>
+            Input.SplitByNewline().Select(v => double.Parse(v)).Sum(CalculateAllRequiredFuel).ToString();
+
+        public double CalculateRequiredFuel(double mass)
+        {
+            var fuelNeeded = Math.Floor(mass / DIVIDE_BY);
+            if (fuelNeeded >= REDUCE_BY)
+            {
+                return fuelNeeded - REDUCE_BY;
+            }
+
+            return 0;
         }
 
-        protected override string SolvePartOne() {
-            return null;
-        }
-
-        protected override string SolvePartTwo() {
-            return null;
+        public double CalculateAllRequiredFuel(double mass)
+        {
+            double requiredFuel = 0;
+            while (mass > 0)
+            {
+                var fuelNeeded = CalculateRequiredFuel(mass);
+                requiredFuel += fuelNeeded;
+                mass = fuelNeeded;
+            }
+            return requiredFuel;
         }
     }
 }
---
+++ Solutions/Year2019/Day01/Tests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace AdventOfCode.Solutions.Year2019
+{
+    public class Tests
+    {
+        private readonly Day01 _day01;
+
+        public Tests()
+        {
+            _day01 = new Day01();
+        }
+
+        [Theory]
+        [InlineData(12, 2)]
+        [InlineData(14, 2)]
+        [InlineData(1969, 654)]
+        [InlineData(100756, 33583)]
+        public void RunExamplesPart1(int mass, int requiredFuel)
+        {
+            Assert.Equal(_day01.CalculateRequiredFuel(mass), requiredFuel);
+        }
+
+        [Theory]
+        [InlineData(14, 2)]
+        [InlineData(1969, 966)]
+        [InlineData(100756, 50346)]
+        public void RunExamplesPart2(int mass, int requiredFuel)
+        {
+            Assert.Equal(_day01.CalculateAllRequiredFuel(mass), requiredFuel);
+        }
+    }
+}
