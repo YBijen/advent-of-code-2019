7f27baa72b32ebbe19021035445126123f62dd3c
Youri Bijen <youri.bijen@iquality.nl>
2020-01-27 08:38:00 +0100

    Added part 1 for day 4

--- Solutions/Year2019/Day04/Solution.cs
+++ Solutions/Year2019/Day04/Solution.cs
@@ -1,17 +1,66 @@
-namespace AdventOfCode.Solutions.Year2019 {
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-    class Day04 : ASolution {
-
+namespace AdventOfCode.Solutions.Year2019
+{
+    class Day04 : ASolution
+    {
         public Day04() : base(4, 2019, "") {
 
         }
 
         protected override string SolvePartOne() {
-            return null;
+            var range = ConvertInputToRange(Input);
+            return range.Count(number => ContainsIncrementingDigits(number) && ContainsDoubleDigits(number)).ToString();
         }
 
         protected override string SolvePartTwo() {
             return null;
+        }
+
+        public bool ContainsIncrementingDigits(int number)
+        {
+            while(number > 0)
+            {
+                // Get the last digit
+                var lastDigit = GetLastDigit(number);
+
+                // Remove the last digit from the number
+                number /= 10;
+
+                // Get the last digit again to compare
+                var compareDigit = GetLastDigit(number);
+
+                if (lastDigit < compareDigit)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int GetLastDigit(int number) => number % 10;
+
+        public bool ContainsDoubleDigits(int number)
+        {
+            var stringNumber = number.ToString();
+            for(var i = 0; i < stringNumber.Length - 1; i++)
+            {
+                if(stringNumber[i] == stringNumber[i + 1])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public IEnumerable<int> ConvertInputToRange(string input)
+        {
+            var values = input.Split("-").Select(v => Convert.ToInt32(v));
+            return Enumerable.Range(values.ElementAt(0), (values.ElementAt(1) - values.ElementAt(0)) + 1);
         }
     }
 }
---
+++ Solutions/Year2019/Day04/Tests.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Solutions.Year2019
+{
+    public class Day04_Tests
+    {
+        private readonly Day04 _day04;
+
+        public Day04_Tests()
+        {
+            _day04 = new Day04();
+        }
+
+        [Fact]
+        public void Test_ConvertInputToRange()
+        {
+            var result = _day04.ConvertInputToRange("120-130");
+            Assert.True(result.Count() == 11);
+            Assert.True(result.First() == 120);
+            Assert.True(result.Last() == 130);
+        }
+
+        [Theory]
+        [InlineData(1234)]
+        [InlineData(1222)]
+        [InlineData(12)]
+        [InlineData(1111)]
+        [InlineData(133)]
+        public void Test_ContainsIncrementingDigits_True(int number)
+        {
+            Assert.True(_day04.ContainsIncrementingDigits(number));
+        }
+
+        [Theory]
+        [InlineData(1243)]
+        [InlineData(1221)]
+        [InlineData(21)]
+        [InlineData(1230)]
+        [InlineData(1001)]
+        public void Test_ContainsIncrementingDigits_False(int number)
+        {
+            Assert.False(_day04.ContainsIncrementingDigits(number));
+        }
+
+        [Theory]
+        [InlineData(1223)]
+        [InlineData(1222)]
+        [InlineData(1111)]
+        [InlineData(1124)]
+        [InlineData(133)]
+        public void Test_ContainsDoubleDigits_True(int number)
+        {
+            Assert.True(_day04.ContainsDoubleDigits(number));
+        }
+
+        [Theory]
+        [InlineData(1245)]
+        [InlineData(1212)]
+        [InlineData(21)]
+        [InlineData(1230)]
+        [InlineData(1021)]
+        public void Test_ContainsDoubleDigits_False(int number)
+        {
+            Assert.False(_day04.ContainsDoubleDigits(number));
+        }
+
+        [Fact]
+        public void Test_ValidRange()
+        {
+            var input = "132-200";
+            var expectedOutput = 7;
+
+            var range = _day04.ConvertInputToRange(input);
+            var actual = range.Count(number => _day04.ContainsIncrementingDigits(number) && _day04.ContainsDoubleDigits(number));
+
+            Assert.Equal(expectedOutput, actual);
+        }
+
+        [Theory]
+        [InlineData(111111)]
+        public void AoCExamples_Part1_True(int number)
+        {
+            Assert.True(_day04.ContainsIncrementingDigits(number) && _day04.ContainsDoubleDigits(number));
+        }
+
+        [Theory]
+        [InlineData(223450)]
+        [InlineData(123789)]
+        public void AoCExamples_Part1_False(int number)
+        {
+            Assert.False(_day04.ContainsIncrementingDigits(number) && _day04.ContainsDoubleDigits(number));
+        }
+    }
+}
