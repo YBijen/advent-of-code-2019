4e61818537e42f81a2778178fbc4d526386ae316
Youri Bijen <youri.bijen@iquality.nl>
2020-01-29 10:49:49 +0100

    Resolved Day 7 part 1 and 2

---
+++ Solutions/Year2019/Day07/IntCodeComputer_Day07_Part2.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions.Year2019
+{
+    /// <summary>
+    /// Copy of the program from Day 5 with a slight modification to the input parameter
+    /// It is now a queue with integers instead of a single integer
+    /// </summary>
+    public class IntCodeComputer_Day07_Part2
+    {
+        private const int OPCODE_ADD = 1;
+        private const int OPCODE_MULTIPLY = 2;
+        private const int OPCODE_PROCESS_INPUT = 3;
+        private const int OPCODE_PROCESS_OUTPUT = 4;
+        private const int OPCODE_JUMP_IF_TRUE = 5;
+        private const int OPCODE_JUMP_IF_FALSE = 6;
+        private const int OPCODE_LESS_THAN = 7;
+        private const int OPCODE_EQUALS = 8;
+        private const int OPCODE_STOP = 99;
+
+        public IntCodeComputer_Day07_Part2(string programInput, int amplifier)
+        {
+            Program = GetProgramFromInput(programInput);
+            Input.Enqueue(amplifier);
+        }
+
+        private List<int> Program;
+
+        private int Output;
+
+        private List<int> GetProgramFromInput(string input) => input.Split(",").Select(v => int.Parse(v)).ToList();
+
+        public Queue<int> Input = new Queue<int>();
+
+        public int CurrentIndex = 0;
+
+        public bool IsStopped = false;
+
+
+        public int RunProgram()
+        {
+            while (true)
+            {
+                var opcode = Program[CurrentIndex];
+                if (opcode == OPCODE_STOP)
+                {
+                    IsStopped = true;
+                    break;
+                }
+
+                var modes = new List<Mode>
+                {
+                    Mode.Position,
+                    Mode.Position,
+                    Mode.Position // Adding the third variable even though I don't expect it to be in Immediate mode ever
+                };
+
+                if (IsParameterMode(opcode))
+                {
+                    modes = UpdateParameterModes(modes, opcode);
+                    opcode = GetOpcodeFromParameter(opcode);
+                }
+
+                Program = opcode switch
+                {
+                    OPCODE_ADD => HandleOpcodeAdd(Program, modes, CurrentIndex),
+                    OPCODE_MULTIPLY => HandleOpcodeMultiply(Program, modes, CurrentIndex),
+                    OPCODE_PROCESS_INPUT => HandleProcessInput(Program, CurrentIndex, Input.Dequeue()),
+                    OPCODE_PROCESS_OUTPUT => HandleProcessOutput(Program, modes, CurrentIndex),
+                    OPCODE_JUMP_IF_TRUE => Program, // Do nothing
+                    OPCODE_JUMP_IF_FALSE => Program, // Do nothing
+                    OPCODE_LESS_THAN => HandleLessThan(Program, modes, CurrentIndex),
+                    OPCODE_EQUALS => HandleEquals(Program, modes, CurrentIndex),
+                    _ => throw new Exception($"Opcode {opcode} is not implemented")
+                };
+
+                // Find the increment for the current opcode
+                CurrentIndex = opcode switch
+                {
+                    OPCODE_ADD => CurrentIndex += 4,
+                    OPCODE_MULTIPLY => CurrentIndex += 4,
+                    OPCODE_PROCESS_INPUT => CurrentIndex += 2,
+                    OPCODE_PROCESS_OUTPUT => CurrentIndex += 2,
+                    OPCODE_JUMP_IF_TRUE => FindIncrementForJumpIfTrue(Program, modes, CurrentIndex),
+                    OPCODE_JUMP_IF_FALSE => FindIncrementForJumpIfFalse(Program, modes, CurrentIndex),
+                    OPCODE_LESS_THAN => CurrentIndex += 4,
+                    OPCODE_EQUALS => CurrentIndex += 4,
+                    _ => throw new Exception($"Opcode {opcode} is not implemented")
+                };
+
+                if(opcode == OPCODE_PROCESS_OUTPUT)
+                {
+                    break;
+                }
+            }
+            return Output;
+        }
+
+        private bool IsParameterMode(int opcode) => opcode.ToString().Length >= 2;
+
+        private int GetOpcodeFromParameter(int parameter) => Convert.ToInt32(parameter.ToString().Substring(parameter.ToString().Length - 2));
+
+        private List<Mode> UpdateParameterModes(List<Mode> currentModes, int parameter)
+        {
+            var str = parameter.ToString().PadLeft(5, '0');
+
+            // Simple check to make sure that the logic is working.
+            // For now I don't expect the input to require the 3rd parameter to support Immediate mode
+            if (str[0] != '0')
+            {
+                throw new Exception("The unexpected happened!");
+            }
+
+            for (var i = currentModes.Count - 1; i >= 0; i--)
+            {
+                currentModes[(currentModes.Count - 1) - i] = str[i] == '0' ? Mode.Position : Mode.Immediate;
+            }
+            return currentModes;
+        }
+
+        private List<int> HandleOpcodeAdd(List<int> program, List<Mode> modes, int currentIndex)
+        {
+            var value1 = GetValueForParameter(ref program, currentIndex + 1, modes[0]);
+            var value2 = GetValueForParameter(ref program, currentIndex + 2, modes[1]);
+            var updateIndex = program[currentIndex + 3];
+            program[updateIndex] = value1 + value2;
+            return program;
+        }
+
+        private List<int> HandleOpcodeMultiply(List<int> program, List<Mode> modes, int currentIndex)
+        {
+            var value1 = GetValueForParameter(ref program, currentIndex + 1, modes[0]);
+            var value2 = GetValueForParameter(ref program, currentIndex + 2, modes[1]);
+            var updateIndex = program[currentIndex + 3];
+            program[updateIndex] = value1 * value2;
+            return program;
+        }
+
+        private List<int> HandleProcessInput(List<int> program, int currentIndex, int inputModifier)
+        {
+            program[program[currentIndex + 1]] = inputModifier;
+            return program;
+        }
+
+        private List<int> HandleProcessOutput(List<int> program, List<Mode> modes, int currentIndex)
+        {
+            Output = GetValueForParameter(ref program, currentIndex + 1, modes[0]);
+            return program;
+        }
+
+        private List<int> HandleLessThan(List<int> program, List<Mode> modes, int currentIndex)
+        {
+            var value1 = GetValueForParameter(ref program, currentIndex + 1, modes[0]);
+            var value2 = GetValueForParameter(ref program, currentIndex + 2, modes[1]);
+            program[program[currentIndex + 3]] = value1 < value2 ? 1 : 0;
+            return program;
+        }
+
+        private List<int> HandleEquals(List<int> program, List<Mode> modes, int currentIndex)
+        {
+            var value1 = GetValueForParameter(ref program, currentIndex + 1, modes[0]);
+            var value2 = GetValueForParameter(ref program, currentIndex + 2, modes[1]);
+            program[program[currentIndex + 3]] = value1 == value2 ? 1 : 0;
+            return program;
+        }
+
+        private int GetValueForParameter(ref List<int> program, int index, Mode mode) => mode switch
+        {
+            Mode.Position => program[program[index]],
+            Mode.Immediate => program[index],
+            _ => throw new Exception($"Missing mode: {mode}")
+        };
+
+        private int FindIncrementForJumpIfTrue(List<int> program, List<Mode> modes, int currentIndex)
+        {
+            if (GetValueForParameter(ref program, currentIndex + 1, modes[0]) != 0)
+            {
+                return GetValueForParameter(ref program, currentIndex + 2, modes[1]);
+            }
+            return currentIndex += 3;
+        }
+
+        private int FindIncrementForJumpIfFalse(List<int> program, List<Mode> modes, int currentIndex)
+        {
+            if (GetValueForParameter(ref program, currentIndex + 1, modes[0]) == 0)
+            {
+                return GetValueForParameter(ref program, currentIndex + 2, modes[1]);
+            }
+            return currentIndex += 3;
+        }
+    }
+}
--- Solutions/Year2019/Day07/Solution.cs
+++ Solutions/Year2019/Day07/Solution.cs
@@ -1,15 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2019
 {
     class Day07 : ASolution
     {
-        public Day07() : base(7, 2019, "") { }
+        private readonly IntCodeComputer_Day07 _intcodeComptuer;
 
-        protected override string SolvePartOne() {
-            return null;
+        public Day07() : base(7, 2019, "")
+        {
+            _intcodeComptuer = new IntCodeComputer_Day07();
         }
 
-        protected override string SolvePartTwo() {
-            return null;
+        protected override string SolvePartOne() => RunProgramForGivenInput(Input).ToString();
+
+        protected override string SolvePartTwo() => RunProgramForGivenInputWithFeedbackloop(Input).ToString();
+
+        public int RunProgramForGivenInputWithFeedbackloop(string input)
+        {
+            var itemsToPermuate = new List<int> { 5, 6, 7, 8, 9 };
+            var permutationList = GetPermutations(itemsToPermuate, itemsToPermuate.Count);
+
+            // Keep track of the highest output
+            var highestOutput = 0;
+
+            foreach (var permutation in permutationList)
+            {
+                var output = 0;
+
+                // For each value in the permutation we create a computer and supply the permutation value
+                var intcodeComputers = permutation.Select(p => new IntCodeComputer_Day07_Part2(input, p)).ToList();
+
+                // Continue running the amplifiers until they are all stopped
+                while(intcodeComputers.Any(ic => !ic.IsStopped))
+                {
+                    foreach(var c in intcodeComputers.Where(x => !x.IsStopped))
+                    {
+                        c.Input.Enqueue(output);
+                        output = c.RunProgram();
+                    }
+                }
+
+                if (output > highestOutput)
+                {
+                    highestOutput = output;
+                }
+            }
+
+            return highestOutput;
+        }
+
+        public int RunProgramForGivenInput(string programInput)
+        {
+            var itemsToPermuate = new List<int> { 0, 1, 2, 3, 4 };
+            var permutationList = GetPermutations(itemsToPermuate, itemsToPermuate.Count);
+
+            // Keep track of the highest output
+            var highestOutput = 0;
+
+            // The queue which will be used as input for the intcodecomputer
+            var intCodeInput = new Queue<int>();
+
+            foreach (var permutation in permutationList)
+            {
+                var output = 0;
+                foreach (var ampSetting in permutation)
+                {
+                    intCodeInput.Enqueue(ampSetting);
+                    intCodeInput.Enqueue(output);
+
+                    output = _intcodeComptuer.RunProgram(programInput, intCodeInput);
+
+                    // A simple check to make sure that each program runs correctly
+                    if (intCodeInput.Count != 0)
+                    {
+                        throw new Exception("Something is left in the queue!");
+                    }
+                }
+
+                if (output > highestOutput)
+                {
+                    highestOutput = output;
+                }
+            }
+
+            return highestOutput;
+        }
+
+        /// <summary>
+        /// Method "stolen" from: https://stackoverflow.com/questions/1952153/what-is-the-best-way-to-find-all-combinations-of-items-in-an-array
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        private IEnumerable<IList<T>> GetPermutations<T>(ICollection<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            // Continue reducing the list until we can create a list of lists
+            return GetPermutations(list, length - 1)
+                // Go through each list of values and check if the other list items are already added to these new lists
+                // If a list is missing the value then we add it by concatting it
+                .SelectMany(t => list.Where(e => !t.Contains(e)), (t1, t2) => t1.Concat(new T[] { t2 }).ToList());
         }
     }
 }
