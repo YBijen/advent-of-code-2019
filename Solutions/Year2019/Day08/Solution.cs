fb1b4aa1af6ef2792d91ff56ee2a93078889f00c
Youri Bijen <youri.bijen@iquality.nl>
2020-01-29 11:46:35 +0100

    Implemented day 8 part 1

--- Solutions/Year2019/Day08/Solution.cs
+++ Solutions/Year2019/Day08/Solution.cs
@@ -1,17 +1,49 @@
-namespace AdventOfCode.Solutions.Year2019 {
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-    class Day08 : ASolution {
+namespace AdventOfCode.Solutions.Year2019
+{
+    class Day08 : ASolution
+    {
+        public Day08() : base(8, 2019, "") { }
 
-        public Day08() : base(8, 2019, "") {
-
-        }
-
-        protected override string SolvePartOne() {
-            return null;
+        protected override string SolvePartOne()
+        {
+            var layers = ConvertImageIntoLayers(Input, 25, 6);
+            var layerWithFewest0 = GetLayerWithFewestAmountOfCharacter(layers, '0');
+            var amountOf1InLayer = FindAmountOfCharsInLayer(layerWithFewest0, '1');
+            var amountOf2InLayer = FindAmountOfCharsInLayer(layerWithFewest0, '2');
+            return (amountOf1InLayer * amountOf2InLayer).ToString();
         }
 
         protected override string SolvePartTwo() {
             return null;
+        }
+
+        public int FindAmountOfCharsInLayer(string layer, char value) => layer.Count(v => v == value);
+
+        public string GetLayerWithFewestAmountOfCharacter(IEnumerable<string> layers, char value) =>
+            layers.Aggregate((l1, l2) => l1.Count(v => v == value) < l2.Count(v => v == value) ? l1 : l2);
+
+        public IEnumerable<string> ConvertImageIntoLayers(string input, int width, int length)
+        {
+            var createdBatches = 0;
+            var batchSize = width * length;
+            var maximumBatches = input.Length / batchSize;
+            while(createdBatches < maximumBatches)
+            {
+                var batchSizeModifier = batchSize * createdBatches;
+
+                var sb = new StringBuilder();
+                for(var i = batchSizeModifier; i < batchSize + batchSizeModifier; i++)
+                {
+                    sb.Append(input[i]);
+                }
+
+                yield return sb.ToString();
+                createdBatches++;
+            }
         }
     }
 }
---
+++ Solutions/Year2019/Day08/Tests.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Solutions.Year2019
+{
+    public class Day08_Tests
+    {
+        private readonly Day08 _day08;
+
+        public Day08_Tests()
+        {
+            _day08 = new Day08();
+        }
+
+        [Fact]
+        public void Test_Grouping()
+        {
+            var image = "123456789012";
+            var layers = _day08.ConvertImageIntoLayers(image, 3, 2);
+
+            Assert.Equal(2, layers.Count());
+            Assert.Equal("123456", layers.ElementAt(0));
+            Assert.Equal("789012", layers.ElementAt(1));
+        }
+
+        [Fact]
+        public void Test_GetLayerWithFewestAmountOfCharacters()
+        {
+            var image = "111456789012";
+            var layers = _day08.ConvertImageIntoLayers(image, 3, 2);
+
+            Assert.Equal("111456", _day08.GetLayerWithFewestAmountOfCharacter(layers, '1'));
+        }
+
+        [Fact]
+        public void Test_FindAmountOfCharsInLayer()
+        {
+            var image = "111456789012";
+            var layers = _day08.ConvertImageIntoLayers(image, 3, 2);
+            var layer = layers.First();
+
+            Assert.Equal(3, _day08.FindAmountOfCharsInLayer(layer, '1'));
+        }
+
+        [Fact]
+        public void Test_Part1()
+        {
+            Assert.Equal("0", _day08.Part1);
+        }
+    }
+}
+
