91fd0cd07aaaff728a47df57194a5339f0165bcd
Youri Bijen <youri.bijen@iquality.nl>
2020-01-30 07:59:08 +0100

    Implemented part 2 of day 8

--- Solutions/Year2019/Day08/Solution.cs
+++ Solutions/Year2019/Day08/Solution.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,10 @@ namespace AdventOfCode.Solutions.Year2019
 {
     class Day08 : ASolution
     {
+        private const char PIXEL_BLACK = '0';
+        private const char PIXEL_WHITE = '1';
+        private const char PIXEL_TRANSPARENT = '2';
+
         public Day08() : base(8, 2019, "") { }
 
         protected override string SolvePartOne()
@@ -17,8 +22,10 @@ namespace AdventOfCode.Solutions.Year2019
             return (amountOf1InLayer * amountOf2InLayer).ToString();
         }
 
-        protected override string SolvePartTwo() {
-            return null;
+        protected override string SolvePartTwo()
+        {
+            var layers = ConvertImageIntoLayers(Input, 25, 6);
+            return "\n" + DecodeImage(layers.ToList(), 25, 6);
         }
 
         public int FindAmountOfCharsInLayer(string layer, char value) => layer.Count(v => v == value);
@@ -44,6 +51,43 @@ namespace AdventOfCode.Solutions.Year2019
                 yield return sb.ToString();
                 createdBatches++;
             }
+        }
+
+        public string DecodeImage(List<string> layers, int width, int length)
+        {
+            var image = new StringBuilder();
+            var sizeOfSingleLayer = width * length;
+
+            var currentWidth = 0;
+            for(var i = 0; i < sizeOfSingleLayer; i++)
+            {
+                foreach(var layer in layers)
+                {
+                    if(layer[i] == PIXEL_TRANSPARENT)
+                    {
+                        continue;
+                    }
+                    else if (layer[i] == PIXEL_BLACK)
+                    {
+                        image.Append(' ');
+                    }
+                    else if(layer[i] == PIXEL_WHITE)
+                    {
+                        image.Append('#');
+                    }
+
+                    currentWidth++;
+                    if (currentWidth == width)
+                    {
+                        currentWidth = 0;
+                        image.Append("\n");
+                    }
+
+                    break;
+                }
+            }
+
+            return image.ToString();
         }
     }
 }
--- Solutions/Year2019/Day08/Tests.cs
+++ Solutions/Year2019/Day08/Tests.cs
@@ -29,7 +29,7 @@ namespace AdventOfCode.Solutions.Year2019
             var image = "111456789012";
             var layers = _day08.ConvertImageIntoLayers(image, 3, 2);
 
-            Assert.Equal("111456", _day08.GetLayerWithFewestAmountOfCharacter(layers, '1'));
+            Assert.Equal("789012", _day08.GetLayerWithFewestAmountOfCharacter(layers, '1'));
         }
 
         [Fact]
@@ -45,7 +45,20 @@ namespace AdventOfCode.Solutions.Year2019
         [Fact]
         public void Test_Part1()
         {
-            Assert.Equal("0", _day08.Part1);
+            Assert.Equal("1463", _day08.Part1);
+        }
+
+        [Fact]
+        public void Test_DecodeImage()
+        {
+            var image = "0222112222120000";
+            var width = 2;
+            var length = 2;
+
+            var layers = _day08.ConvertImageIntoLayers(image, width, length);
+            var decodedImage = _day08.DecodeImage(layers.ToList(), width, length);
+
+            Assert.Equal(" #\n# \n", decodedImage);
         }
     }
 }
